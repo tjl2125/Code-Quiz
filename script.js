5990ec459e522acd687e3fefd84d606fa69cd201
Taylor Leong <tjl2125@alum.barnard.edu>
2021-02-23 15:58:33 -0500

    added more divs and functions,tbd if they work

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 var startButton = document.getElementById("start-btn"); 
 var hideaway = document.getElementsByClassName("hide"); 
+var questionEl = document.getElementById("question"); 
+var answerBtnEl= document.getElementById("answer-btns")
 
+var currentQ = {}
+var score = 0
+var questionCount= 0
+var availableQ = []
 
+var ScorePoints = 100
+var MaxQ= 4
 
 startButton.addEventListener('click', startGame);
-//startButton.addEventListener('click',hide); 
+
 
 
 function startGame() {
 console.log("start"); 
 document.getElementById("home-page-container").style.visibility = "hidden";
 nextQuestion(); 
-}
 
-function nextQuestion() {
 
 }
+
+function nextQuestion(question) {
+    if (availableQ.length === 0 || questionCounter > MaxQ){
+        localStorage.setItem("mostRecentScore", score)
+        return window.location
+    }
+    questionCount++
+    //progressText.innerText = "Question {questionCount} of {Max_Questions}"
+
+    let questionIndex = Math.floor(Math.random() * availableQ.length)
+    currentQ = availableQ[questionsIndex]
+    question.innerText = currentQ.question
+    
+    choices.forEach(choices => {
+        var number = choice.dataset["number"]
+        choice.innerText = currentQ["choice" + number]
+    })
+    availableQ.splice(questionsIndex,1)
+
+    acceptingAnswer=true 
+}
+
+/*choices.forEach (choices => {
+    choice.addEventListener("click", e =>{
+        if (!acceptingAnswer) return
+        acceptingAnswer = false 
+        var selectedChoice = e.target
+        var selectedAnswer = selectedChoice.dataset["number"]
+
+        let apply = selectedAnswer == currentQ.answer
+    })
+})*/
 
 function pickAnswer() {
 
@@ -55,3 +93,4 @@ var questions = [
         answer: 4,
     },
 ]
+
