27c1cada807546a3b54cdcb0853fc1595d89b6af
Taylor Leong <tjl2125@alum.barnard.edu>
2021-02-25 18:45:22 -0500

    rearranging functions

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var startButton = document.getElementById("start-btn"); 
+var homePage = document.getElementById("home-page-container");
+var quiz = document.getElementById("quiz"); 
 var hideaway = document.getElementsByClassName("hide"); 
 var questionEl = document.getElementById("question"); 
 var answerBtnEl= document.getElementById("answer-btns")
 
-var currentQ = {}
+var choiceA= document.getElementById("A"); 
+var choiceB= document.getElementById("B"); 
+var choiceC= document.getElementById("C"); 
+var choiceD= document.getElementById("D"); 
+
+/*var currentQ = {}
 var score = 0
 var questionCount= 0
 var availableQ = []
-
-var ScorePoints = 100
-var MaxQ= 4
 
 startButton.addEventListener('click', startGame);
 
@@ -55,43 +59,96 @@ function nextQuestion(question) {
     })
 })*/
 
-function pickAnswer() {
+startButton.addEventListener("click", startQuiz);
 
+function startQuiz () {
+    startButton.style.display = "none"; 
+    counterRender(); 
+    Timer = setInterval(counterRender,1000); 
 }
 
 var questions = [
     {
         question: "What is the first letter of the alphabet?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
         answer: 1,
     },
     {
         question: "What is the second letter of the alphabet?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
         answer: 2,
     },
     {
         question: "What is the third letter of the alphabet?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
         answer: 3,
     },
     {
         question: "What is the fourth letter of the alphabet?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
         answer: 4,
     },
 ]
 
+var questionIndex = question.length-1; 
+var currentQuestion= 0; 
 
+function renderQuestion() {
+    var q = questions[currentQuestion];
+    questionEl.innerHTML = q.questionEl; 
+    choiceA.innterHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD; 
+}
+
+var questionTime = 20; 
+var count = 0; 
+
+function counterRender () {
+    if (count <= questionTime) {
+        counter.innerHtml = count;
+        count++;
+    }
+    else {
+        count = 0;
+        answerWrong();
+        if (currentQuestion <= questionIndex) {
+            currentQuestion ++; 
+            questionRender();
+        } else {
+            clearInterval(Timer);
+            scoreRender(); 
+        }
+    }
+}
+var Timer = setInterval(counterRender,1000);
+
+function checkAnswer(answer) {
+    if questions[currentQuestion].correct == answer) {
+        score++; 
+        answerCorrect();
+    } else {
+        answerWrong();
+    }
+    if(currentQuestion < questionIndex) {
+        count = 0;
+        currentQuestion++; 
+        questionRender();
+    } else {
+        clearInterval(Timer);
+        scoreRender(); 
+    }
+}
