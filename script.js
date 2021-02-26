5ae8a13b465bb8b722d02cf9be5fa355306e2fd5
Taylor Leong <tjl2125@alum.barnard.edu>
2021-02-26 16:05:53 -0500

    whole code but some errors

--- script.js
+++ script.js
@@ -3,69 +3,16 @@ var homePage = document.getElementById("home-page-container");
 var quiz = document.getElementById("quiz"); 
 var hideaway = document.getElementsByClassName("hide"); 
 var questionEl = document.getElementById("question"); 
-var answerBtnEl= document.getElementById("answer-btns")
+var answerBtnEl= document.getElementById("answer-btns");
+var questionCorrect = document.getElementById("correct");
+var counter = document.getElementById("counter");
+var scoreDiv = document.getElementById("score"); 
 
 var choiceA= document.getElementById("A"); 
 var choiceB= document.getElementById("B"); 
 var choiceC= document.getElementById("C"); 
 var choiceD= document.getElementById("D"); 
 
-/*var currentQ = {}
-var score = 0
-var questionCount= 0
-var availableQ = []
-
-startButton.addEventListener('click', startGame);
-
-
-
-function startGame() {
-console.log("start"); 
-document.getElementById("home-page-container").style.visibility = "hidden";
-nextQuestion(); 
-
-
-}
-
-function nextQuestion(question) {
-    if (availableQ.length === 0 || questionCounter > MaxQ){
-        localStorage.setItem("mostRecentScore", score)
-        return window.location
-    }
-    questionCount++
-    //progressText.innerText = "Question {questionCount} of {Max_Questions}"
-
-    let questionIndex = Math.floor(Math.random() * availableQ.length)
-    currentQ = availableQ[questionsIndex]
-    question.innerText = currentQ.question
-    
-    choices.forEach(choices => {
-        var number = choice.dataset["number"]
-        choice.innerText = currentQ["choice" + number]
-    })
-    availableQ.splice(questionsIndex,1)
-
-    acceptingAnswer=true 
-}
-
-/*choices.forEach (choices => {
-    choice.addEventListener("click", e =>{
-        if (!acceptingAnswer) return
-        acceptingAnswer = false 
-        var selectedChoice = e.target
-        var selectedAnswer = selectedChoice.dataset["number"]
-
-        let apply = selectedAnswer == currentQ.answer
-    })
-})*/
-
-startButton.addEventListener("click", startQuiz);
-
-function startQuiz () {
-    startButton.style.display = "none"; 
-    counterRender(); 
-    Timer = setInterval(counterRender,1000); 
-}
 
 var questions = [
     {
@@ -99,19 +46,30 @@ var questions = [
         choiceC: "C",
         choiceD: "D",
         answer: 4,
-    },
-]
+    }
+]; 
 
-var questionIndex = question.length-1; 
-var currentQuestion= 0; 
+var questionIndex = question.length-1; //lastQuestion
+var currentQuestion= 0; //runningQuestion
 
 function renderQuestion() {
-    var q = questions[currentQuestion];
-    questionEl.innerHTML = q.questionEl; 
-    choiceA.innterHTML = q.choiceA;
+    let q = questions[currentQuestion];
+
+    questionEl.innerHTML = "<p>" + q.questionEl + "<p>"; 
+    choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
     choiceD.innerHTML = q.choiceD; 
+}
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz () {
+    startButton.style.display = "none"; 
+    renderQuestion(); 
+    quiz.style.display  = "block"; 
+    counterRender(); 
+    Timer = setInterval(counterRender,1000); 
 }
 
 var questionTime = 20; 
@@ -121,13 +79,13 @@ function counterRender () {
     if (count <= questionTime) {
         counter.innerHtml = count;
         count++;
-    }
+    } 
     else {
         count = 0;
         answerWrong();
         if (currentQuestion <= questionIndex) {
             currentQuestion ++; 
-            questionRender();
+            renderQuestion();
         } else {
             clearInterval(Timer);
             scoreRender(); 
@@ -135,20 +93,33 @@ function counterRender () {
     }
 }
 var Timer = setInterval(counterRender,1000);
+var score = 0; 
 
 function checkAnswer(answer) {
-    if questions[currentQuestion].correct == answer) {
+    if (answer == questions[currentQuestion].correct) {
         score++; 
         answerCorrect();
     } else {
         answerWrong();
     }
+    count = 0;
     if(currentQuestion < questionIndex) {
-        count = 0;
         currentQuestion++; 
-        questionRender();
+        renderQuestion();
     } else {
         clearInterval(Timer);
         scoreRender(); 
     }
 }
+
+function answerCorrect() {
+    document.getElementById("correct").style.backgroundColor = "#0f0"; 
+}
+function answerWrong() {
+    document.getElementById("wrong").style.backgroundColor= "#f00"; 
+}
+function scoreRender() {
+    scoreDiv.style.display = "block"; 
+    
+}
+
