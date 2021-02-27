54e17993528cd97a6c25e9188a2ae44d5076af74
Taylor Leong <tjl2125@alum.barnard.edu>
2021-02-27 16:22:48 -0500

    fixing functions

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-var startButton = document.getElementById("start-btn"); 
+var start = document.getElementById("home-page-container");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var counter = document.getElementById("counter");
+var progress = document.getElementById("progress");
+var scoreDiv = document.getElementById("scoreContainer");
+var correct = document.getElementById("correct");
+var wrong = document.getElementById("wrong"); 
+var leaderboard = JSON.parse(localStorage.getItem("leaderboard")) || [];
+
+//older below
+/*var startButton = document.getElementById("start-btn"); 
 var homePage = document.getElementById("home-page-container");
 var quiz = document.getElementById("quiz"); 
 var questionEl = document.getElementById("question"); 
 var answerBtnEl= document.getElementById("answer-btns");
 var questionCorrect = document.getElementById("correct");
 var counter = document.getElementById("counter");
-var scoreDiv = document.getElementById("score"); 
+var scoreDiv = document.getElementById("score"); */
 
 var choiceA= document.getElementById("A"); 
 var choiceB= document.getElementById("B"); 
@@ -15,149 +26,229 @@ var choiceD= document.getElementById("D");
 
 var questions = [
     {
-        question: "What is the first letter of the alphabet?",
-        choiceA: "A",
-        choiceB: "B",
-        choiceC: "C",
-        choiceD: "D",
-        answer: 1,
+        question : "What does HTML stand for?",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        choiceD : "Wrong",
+        correct : "A"
     },
     {
-        question: "What is the second letter of the alphabet?",
-        choiceA: "A",
-        choiceB: "B",
-        choiceC: "C",
-        choiceD: "D",
-        answer: 2,
+        question : "What does CSS stand for?",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        choiceD : "Wrong",
+        correct : "B"
     },
     {
-        question: "What is the third letter of the alphabet?",
-        choiceA: "A",
-        choiceB: "B",
-        choiceC: "C",
-        choiceD: "D",
-        answer: 3,
+        question : "What does JS stand for?",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        choiceD : "Wrong", 
+        correct : "C"
     },
     {
-        question: "What is the fourth letter of the alphabet?",
+        question: "What is the third letter in the alphabet?",
         choiceA: "A",
         choiceB: "B",
         choiceC: "C",
         choiceD: "D",
-        answer: 4,
+        correct: "C"
+    },
+    {
+        question: "What is the fourth letter in the alphabet?",
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "Correct",
+        correct: "D"  
     }
-]; 
 
+];
 
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
-let score = 0;
+var lastQuestion = questions.length - 1;
+var runningQuestion = 0;
+var count = 30;
+var questionTime = 0; // 30s
+var timer;
+var score = 0;
 
-// render a question
-function renderQuestion(){
+//generates question from array
+function produceQuestion(){
     let q = questions[runningQuestion];
     
-    questionEl.innerHTML = "<p>"+ q.questionEl +"</p>";
+    question.innerHTML = "<p>"+ q.question +"</p>";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
     choiceD.innerHTML = q.choiceD; 
 }
 
-startButton.addEventListener("click",startQuiz);
+start.addEventListener("click",startQuiz);
 
 // start quiz
 function startQuiz(){
-    startButton.style.display = "none";
-    renderQuestion();
+    start.style.display = "none";
+    produceQuestion();
     quiz.style.display = "block";
-    //renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    renderProgress();
+    createCounter();
+    timer = setInterval(createCounter,1000); // 1000ms = 1s
+    gameOver(); 
 }
+
+//saving leaderboard
+function highScore () {
+    leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
+    leaderboard.sort((a, b) => b.score - a.score);
+    for (var i = 0; (i < 1); i++) {
+        var list = document.createElement('li');
+        highScoreDisplay.textContent = leaderboard[i].name + " : " + leaderboard[i].score;
+    }
+};
 
 // render progress
-/*function renderProgress(){
+function renderProgress(){
     for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-    }
-}*/
-
-// counter render
-
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        //timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }else{
-        count = 0;
-        // change progress color to red
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            // end the quiz and show the score
-            clearInterval(TIMER);
-            scoreRender();
-        }
+       progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
     }
 }
 
-// checkAnwer
+// create counter
+function createCounter(){
+    if(count >= questionTime){
+        counter.innerHTML = count;
+        count--;
+    } 
+}
 
+// check if answer is right or wrong
 function checkAnswer(answer){
     if( answer == questions[runningQuestion].correct){
         // answer is correct
         score++;
-        // change progress color to green
         answerIsCorrect();
     }else{
         // answer is wrong
-        // change progress color to red
         answerIsWrong();
     }
-    count = 0;
     if(runningQuestion < lastQuestion){
         runningQuestion++;
-        renderQuestion();
+        produceQuestion();
     }else{
         // end the quiz and show the score
-        clearInterval(TIMER);
+        clearInterval(timer);
         scoreRender();
+        document.getElementById("yourName").style.display = "block"; 
     }
-}
+}; 
 
 // answer is correct
 function answerIsCorrect(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+    correct.innerHTML = "Correct!";
+    window.setTimeout("disappearCorrect();", 500);
 }
 
-// answer is Wrong
+function disappearCorrect(){
+    document.getElementById("correct").style.display = "none";
+}
+
+// answer is wrong, also decreases time if wrong
 function answerIsWrong(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+    wrong.innerHTML = "Wrong!"; 
+    window.setTimeout("disappearWrong();", 500);
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        produceQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(timer);
+        scoreRender();
+    }
+    if (count >= 0) {
+        count -=5; 
+    }
+}
+
+function disappearWrong() {
+    document.getElementById("wrong").style.display = "none"; 
 }
 
 // score render
-function scoreRender(){
+function scoreRender(){ 
+    quiz.style.display = "none";
     scoreDiv.style.display = "block";
+    score;
+    scoreDiv.innerHTML += "<p>"+ score +"</p>";
     
-    // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
-    
-    // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
-              "img/1.png";
-    
-    scoreDiv.innerHTML = "<img src="+ img +">";
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
-}
+} ; 
+
+function gameOver() {
+    // define and create name submit form elements
+    var target = document.querySelector('#yourName');
+    var form = document.createElement('form');
+    var div = document.createElement('div');
+    // var scoreText = document.createElement('p');
+    var label = document.createElement('label');
+    var field = document.createElement('input');
+    var submit = document.createElement('button');
+    label.textContent = "Enter your name or initials: ";
+    label.setAttribute("class", "mr-2")
+    // div.setAttribute("class", "row d-flex flex-column justify-content-center")
+    form.id = "#form";
+    label.for = "player";
+    label.type = "text"
+    // scoreText.textContent = `Your final score is ${score}`;
+    submit.id = "submitName";
+    field.id = "player";
+    field.type = "text";
+    field.name = "player";
+    submit.textContent = "Submit";
+    submit.setAttribute("class", "btn-sm btn-success px-4 mt-2");
+    field.setAttribute("class", "form-control");
+    div.setAttribute("class", "form-inline");
+    target.appendChild(form);
+    form.appendChild(div);
+    // div.appendChild(scoreText);
+    div.appendChild(label);
+    div.appendChild(field);
+    target.appendChild(submit);
+    submit.addEventListener("click", saveName);
+};
+
+// Save name if a new high score
+function saveName(event) {
+    event.preventDefault();
+    var playerName = player.value.trim();
+    // if score is higher than previous value of highscore, replace as new value and save new high score
+    if (score > highscore) {
+        highscore = score;
+        highScoreDisplay.textContent = playerName + ", " + score;
+    }
+    var user = {
+        name: playerName,
+        score: score,
+    }
+    leaderboard.push(user);
+    localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
+    //clear all form elements
+    var target = document.querySelector('#yourName');
+    while (target.firstChild) {
+        target.removeChild(target.firstChild);
+    }
+    buildLeaderboard();
+};
+
+function buildLeaderboard() {
+    // Retrieve leaderboard array from local storage, sort it in descending order based on score value, and create a list with the top 4 scores
+    leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
+    leaderboard.sort((a, b) => b.score - a.score);
+    for (var i = 0; (i < leaderboard.length) && (i <4); i++) {
+        var list = document.createElement('li');
+        list.textContent = leaderboard[i].name + " : " + leaderboard[i].score;
+        list.style = "font-size: .7em";
+        container.appendChild(list);
+    }
+}; 
